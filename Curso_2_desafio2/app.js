83d9f0683bec752afe29036c69f2fef3b76ea466
Darc Mary <darcmary12@gmail.com>
2024-01-24 00:17:38 -0300

    Atualizações do desafio 2

--- Curso_2_desafio2/app.js
+++ Curso_2_desafio2/app.js
@@ -11,5 +11,23 @@ hello("Tata");
 function dobro(numero) {
    return numero * 2;   
 }
-const resultado = dobro(8);
+const resultado = dobro(parseInt(Math.random() * 1000) + 1);
 console.log(resultado);
+
+function media(number1, number2, number3) {
+    return (number1 + number2 + number3) / 3;
+}
+const result = media(5, 2, 8);
+console.log(result);
+
+function maior(a, b) {
+    return a > b ? a : b;
+} 
+const oMaiorNumero = maior(18, 19);
+console.log(oMaiorNumero);
+
+function quadrado(number) {
+    return number * number;
+}
+const resultad = quadrado(parseInt(Math.random() * 100) + 1);
+console.log(resultad);
